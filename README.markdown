c8b21360ff86872de033310638d556f45edf75d3
yoshitia <naha.it01@gmail.com>
2019-11-17 14:27:33 +0900

    first commit

---
+++ README.markdown
@@ -0,0 +1,6 @@
+# Yubin
+   this code is from "suvival common lisp"(https://booth.pm/ja/items/1300098).
+
+## Usage
+
+## Installation
---
+++ src/main.lisp
@@ -0,0 +1,22 @@
+(defpackage yubin
+  (:use :cl)
+  (:import-from #:jonathan #:parse)
+  (:export #:get-place))
+(in-package :yubin)
+
+(defun get-place (zipcode)
+  (let* ((url (quri:make-uri :defaults "http://zipcloud.ibsnet.co.jp/api/search"
+                             :query '(("zipcode" . ,zipcode)))) ; QuriでURLを作る
+         (response (parse (dex:get url)))                       ; HTTPリクエストを行う
+         (result (first (getf response :|results|))))
+    (if result
+        (concatnate 'string
+                    (getf result :|address1|)
+                    (getf result :|address2|)
+                    (getf result :|address3|))
+        (error (format nil "~A: ~S (Code=~A)"
+                       (getf response :|message|)
+                       zipcode
+                       (getf response :|status|))))))
+
+;; blah blah blah.
